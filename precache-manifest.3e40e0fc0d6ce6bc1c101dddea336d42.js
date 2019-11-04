540c5bbc92803ebcd94bdc13868fd53881b37d84
Randall <rfields1128@yahoo.com>
2019-11-04 14:45:35 -0600

    Updates

---
+++ precache-manifest.3e40e0fc0d6ce6bc1c101dddea336d42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4f259bbd8e8848607eaad65dde7281",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "55205e86f32c9b2cbaa5",
+    "url": "/clicky-game/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "dcb405b312b8901f0acf",
+    "url": "/clicky-game/static/js/2.d8ad3649.chunk.js"
+  },
+  {
+    "revision": "55205e86f32c9b2cbaa5",
+    "url": "/clicky-game/static/js/main.4d1083a8.chunk.js"
+  },
+  {
+    "revision": "dfc666a7910870542e55",
+    "url": "/clicky-game/static/js/runtime-main.caaefa61.js"
+  }
+]);
